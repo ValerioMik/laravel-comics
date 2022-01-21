cb011ae2e3d3cbb8cf68ab6d7b9277a9a2d7193e
ValerioMik <valerio.avallone@hotmail.com>
2022-01-21 17:26:01 +0100

    secondo step

--- resources/views/component/footer.blade.php
+++ resources/views/component/footer.blade.php
@@ -1,3 +1,46 @@
-<div id="footer">
-    <h1>Footer</h1>
-</div>
+<footer>
+    <div class="socials">
+      <aside id="sx">
+        <div class="linkdisx">
+          <h3>DC COMICS</h3>
+          <ul>
+            <li><a href="">characters</a></li>
+            <li><a href="">comics</a></li>
+            <li><a href="">movies</a></li>
+            <li><a href="">tv</a></li>
+            <li><a href="">games</a></li>
+            <li><a href="">video</a></li>
+            <li><a href="">news</a></li>
+          </ul>
+          <h3>SHOP</h3>
+          <ul>
+            <li><a href="">shops dc</a></li>
+            <li><a href="">shops dc collections</a></li>
+          </ul>
+        </div>
+
+        <div class="linkdidx">
+          <h3>DC</h3>
+          <ul>
+            <li><a href="">terme out</a></li>
+            <li><a href="">pryvacy policy</a></li>
+            <li><a href="">ad choices</a></li>
+            <li><a href="">Adversting</a></li>
+            <li><a href="">jobs</a></li>
+            <li><a href="">subscription</a></li>
+            <li><a href="">talent worckscoop</a></li>
+          </ul>
+          <h3>SITES</h3>
+          <ul>
+            <li><a href="">dc</a></li>
+            <li><a href="">dc magazine</a></li>
+            <li><a href="">dc kids</a></li>
+            <li><a href="">dc bho</a></li>
+          </ul>
+        </div>
+      </aside>
+      <aside id="dx">
+        <img class="bigLogo" src="./public\images\dc-logo.png" />
+      </aside>
+    </div>
+  </footer>
