0342a7924ca046bb9c10ba17805910abc23de80e
ValerioMik <valerio.avallone@hotmail.com>
2022-01-24 16:47:34 +0100

    MODIFICHE VARIE

--- resources/views/fumetti.blade.php
+++ resources/views/fumetti.blade.php
@@ -3,11 +3,16 @@
     <div class="contfumetti">
         <div class="testo">
             <h1>Action Comics #1000:edizione DELUX </h1>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus, error quam? Laboriosam dolorum fuga, rerum
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid esse praesentium enim quisquam dolor! Explicabo deserunt at nostrum, nihil nisi exercitationem odio labore incidunt autem neque magni similique, fugiat consectetur? </p>
+            <div class="green">
+                <h2>US.PRICE : 19,45$</h2>
+            </div>
+            <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus, error quam? Laboriosam dolorum fuga, rerum
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem veritatis est voluptatem sapiente dolores, ipsum voluptas veniam ex architecto reprehenderit eius eos, esse maiores quae quisquam, possimus explicabo laudantium culpa. Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, qui odit eaque corporis placeat magni dolorem voluptates aperiam voluptatem corrupti ab nulla vero, minima, tempore totam. Quisquam tenetur quaerat minima? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam error beatae laborum ex. Omnis impedit rerum illo unde? Atque neque exercitationem quos eveniet, ab ea. Nulla magnam placeat neque quibusdam!
+            </p>
         </div>
         <div class="thumbs_singola">
-            <img class="immagine" src="https://www.mangacomicsmarket.it/app/media/images/numeri/50093721.jpg" alt="wolwerin">
+            <img class="immagine1" src="https://i.pinimg.com/736x/bd/47/b9/bd47b9eee29323912804ece696d3674e.jpg" alt="wolwerin">
             <h2>Wolwerin</h2>
         </div>
     </div>
