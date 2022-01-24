caa87494d3ce8f3360ce8e8b8fea1522d4191c70
ValerioMik <valerio.avallone@hotmail.com>
2022-01-24 16:51:51 +0100

    modifica torna indietro

--- resources/views/fumetti.blade.php
+++ resources/views/fumetti.blade.php
@@ -10,10 +10,16 @@
                 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus, error quam? Laboriosam dolorum fuga, rerum
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem veritatis est voluptatem sapiente dolores, ipsum voluptas veniam ex architecto reprehenderit eius eos, esse maiores quae quisquam, possimus explicabo laudantium culpa. Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, qui odit eaque corporis placeat magni dolorem voluptates aperiam voluptatem corrupti ab nulla vero, minima, tempore totam. Quisquam tenetur quaerat minima? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam error beatae laborum ex. Omnis impedit rerum illo unde? Atque neque exercitationem quos eveniet, ab ea. Nulla magnam placeat neque quibusdam!
             </p>
+            <div>
+                <a href="{{route('home')}}">
+                <button>Torna Indietro</button>
+                </a>
+            </div>
         </div>
         <div class="thumbs_singola">
             <img class="immagine1" src="https://i.pinimg.com/736x/bd/47/b9/bd47b9eee29323912804ece696d3674e.jpg" alt="wolwerin">
             <h2>Wolwerin</h2>
         </div>
+        
     </div>
 @endsection
